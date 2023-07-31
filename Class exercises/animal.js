e393eb8a839be15fdef25ec20d41f7c7379b3b5d
ivanov9826 <ivanov9826@gmail.com>
2023-07-31 16:41:55 +0300

    Adding new common animal class

--- Class exercises/animal.js
+++ Class exercises/animal.js
@@ -42,3 +42,21 @@ class Dolphin extends MotherAnimal {
 const firstDolph = new Dolphin("Ian", 5);
 
 firstDolph.dolphinInfo();
+
+// One class for any kind of animal
+class ChildAnimal extends MotherAnimal {
+  constructor(name, age, type) {
+    super(name, age);
+    this.type = type;
+  }
+
+  animalInfo() {
+    console.log(
+      `Hello , I'm ${this.name} and I'm ${this.age} years old . I am a ${this.type} .`
+    );
+  }
+}
+
+const firstAnimal = new ChildAnimal("Petrahan", 12, "duck");
+
+firstAnimal.animalInfo();
