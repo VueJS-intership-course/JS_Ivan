36f16d65af269b05671bc7c0f607c9660a5b4cbe
ivanov9826 <ivanov9826@gmail.com>
2023-08-02 10:08:08 +0300

    Uploading the infinte scroll task

---
+++ Infinite scroller/app.js
@@ -0,0 +1,43 @@
+const cardContainer = document.querySelector(".card-container");
+const cards = document.querySelectorAll(".card");
+
+function loadNewCards() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement("div");
+    card.textContent = "New Card";
+    card.classList.add("card");
+    observer.observe(card);
+    cardContainer.append(card);
+  }
+}
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+    // rootMargin: "-100px",
+  }
+);
+
+const lastCardObserver = new IntersectionObserver(
+  (entries) => {
+    const lastCard = entries[0];
+    if (!lastCard.isIntersecting) return;
+    loadNewCards();
+    lastCardObserver.unobserve(lastCard.target);
+    lastCardObserver.observe(document.querySelector(".card:last-child"));
+  },
+  {
+    rootMargin: "100px",
+  }
+);
+
+lastCardObserver.observe(document.querySelector(".card:last-child"));
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
